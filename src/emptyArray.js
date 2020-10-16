c4c892dcda49dbd3871e0e986b164338948a0721
zidniryi <zidnirydwan@gmail.com>
2020-10-16 21:41:07 +0700

    feat(create feature to check empty array): create feature check empty array
    create feature to check feature empty array

---
+++ src/emptyArray.js
@@ -0,0 +1,8 @@
+function checkEmptyArray(emptyArray) {
+  if (emptyArray != null && emptyArray.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+export default checkEmptyArray;
